c0ea491c854f3d24adc481544ce3aaaf45753552
Bekzat Akhmetov <b.akhmetov@astanait.edu.kz>
2021-05-10 11:11:45 +0600

    On of

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import './index.css';
 import App from './App';
 import store from "./redux/redux-store";
 import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
     ReactDOM.render(
-        <BrowserRouter>
+        <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-        </BrowserRouter>,document.getElementById('root')
+        </HashRouter>,document.getElementById('root')
     )
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
