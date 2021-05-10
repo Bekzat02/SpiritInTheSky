a245e576e6295def886d780e3d3db4954a7682f4
Bekzat <b.akhmetov@astanait.edu.kz>
2021-05-10 10:32:24 +0600

    Updates

---
+++ precache-manifest.27a8730e4cc9c38c538f7eb99a1f45e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1501eca51aac457db489cf114ffa6617",
+    "url": "/SpiritInTheSky/index.html"
+  },
+  {
+    "revision": "8e7f1254890100322aac",
+    "url": "/SpiritInTheSky/static/css/main.9a98dccc.chunk.css"
+  },
+  {
+    "revision": "c38253257a854eb78663",
+    "url": "/SpiritInTheSky/static/js/2.10555f89.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/SpiritInTheSky/static/js/2.10555f89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e7f1254890100322aac",
+    "url": "/SpiritInTheSky/static/js/main.1ba8f73e.chunk.js"
+  },
+  {
+    "revision": "a91e9f97953f6cf27829",
+    "url": "/SpiritInTheSky/static/js/runtime-main.d38b939d.js"
+  },
+  {
+    "revision": "6e7c9db7e1f100edac7c6e48714d2ea0",
+    "url": "/SpiritInTheSky/static/media/preloader.6e7c9db7.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/SpiritInTheSky/static/media/user.8b1dbb1f.png"
+  }
+]);
